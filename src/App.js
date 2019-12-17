2c6234c435b000387722c58e6cc558ce3ff57ce4
Meet Trivedi <03000272@DHFLGI-03000272.local>
2019-12-17 10:55:02 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -44,13 +44,6 @@ class App extends Component{
   }
 
   render(){
-    const style = {
-      backgroundColor:'white',
-      font:"inherit",
-      border:"1px solid blue",
-      padding:"8px"
-    }
-
     let persons = null
 
     if(this.state.showPersons){
@@ -81,7 +74,7 @@ class App extends Component{
       <div className="App">
         <h1>Hi i am a React app</h1>
         <p className={classes.join(' ')}>This is really working!!!</p>
-        <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
+        <button className="button" onClick={this.togglePersonHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
